3fa04605a836510837dac447e9c286918c038416
aboud20-meet <aboud20@meet.mit.edu>
2018-12-13 19:01:31 +0200

    api post

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests, json
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,12 +11,35 @@ def study_image():
     
     image_url = request.form['url-input']
     # At this point you have the image_url value from the front end
-    # Your job now is to send this information to the Clarifai API
+    # Your job now is to from flask import Flask, render_template, request
+	#send this information to the Clarifai API
     # and read the result, make sure that you read and understand the
     # example we covered in the slides! 
 
     # YOUR CODE HERE!
-    
+
+
+# you can use the headers to pass in hidden info, here we are sending a secret Key (think of it as a password)
+    headers = {'Authorization': 'Key f2f339a3cc374420a221fa27e58a3202'}
+
+# this is the url of where your request will go
+    api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+
+# this is content of the message(data) you are sending to clarifai
+    data ={"inputs": [
+          {
+            "data": {
+              "image": {
+                "url": ""
+              }
+            }
+          }
+        ]}
+
+# putting everything together; sending the request!
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+
+
     return render_template('home.html', results="No results yet :(")
 
 if __name__ == '__main__':
